1e6211bca5b0b8971d45c20316177c09e711d169
Alexander Karpov <alexander.karpov@neosperience.com>
2016-03-28 12:18:02 +0200

    Added lesson 1

---
+++ Lesson_HomeWork/Lesson1/Lesson1_HomeWork_iosDevCourseSwift.playground/Contents.swift
@@ -0,0 +1,12 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+let firstName = "Alexander"
+let lastName = "Karpov"
+let age = 30
+let gender = "Male"
+let university = "University of Urbino"
+let passion = "Programming"
+
+print("My name is \(firstName)\(lastName). I'm \(gender) of \(age) years old. I have studied at \(university) and i love \(passion)")
---
+++ Lesson_HomeWork/Lesson1/Lesson1_iosDevCourseSwift.playground/Contents.swift
@@ -0,0 +1,32 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+let redColor = "red"
+var color = "black"
+
+color = redColor
+
+print("Red color value = \(redColor)\nGreen color value")
+
+//let minValue = UInt8.min 
+//let maxValue = UInt8.max
+
+let minValue = UInt64.min
+let maxValue = UInt64.max
+
+
+
+2016-03-21 09:19:29.244 TTS-Trieste[3298:1177537] connectionShouldUseCredentialStorage
+2016-03-21 09:19:29.362 TTS-Trieste[3298:1177537] shouldStartLoadWithRequest
+2016-03-21 09:19:29.363 TTS-Trieste[3298:1177537] webViewDidStartLoad
+2016-03-21 09:19:33.568 TTS-Trieste[3298:1177537] didReceiveAuthenticationChallenge
+2016-03-21 09:19:34.494 TTS-Trieste[3298:1177537] shouldStartLoadWithRequest
+
+
+2016-03-21 09:21:36.970 TTS-Trieste[3298:1177537] connectionShouldUseCredentialStorage
+2016-03-21 09:21:36.976 TTS-Trieste[3298:1177537] shouldStartLoadWithRequest
+2016-03-21 09:21:36.977 TTS-Trieste[3298:1177537] webViewDidStartLoad
+2016-03-21 09:21:42.472 TTS-Trieste[3298:1177537] shouldStartLoadWithRequest
+2016-03-21 09:21:42.532 TTS-Trieste[3298:1177537] didReceiveAuthenticationChallenge
+2016-03-21 09:21:53.871 TTS-Trieste[3298:1177537] webViewDidFinishLoad
